c274f99deddb9dc61e576435b943d4ba1f9698f3
Tim Hinrichs <timothy.l.hinrichs@gmail.com>
2016-07-13 16:46:18 -0700

    Disable old DSE code
    - Move tests2 to tests
    - Disabled a few tests that require porting to new DSE
      and marked with TODO(dse2)
    - Removed all the distributed_architecture checks
    - Fixed api_driver_model and upgraded tests to dist_arch
    
    Still need to remove dse code and deal with fallout
    Still need to remove dead code from some api models
    But this patchset is large enough, so save rest for
    future work.
    
    Change-Id: Ia7688b86186e321cfd349ebae1f2ec6c9068616c

--- congress/server/congress_server.py
+++ congress/server/congress_server.py
@@ -151,8 +151,6 @@ def main():
     deploy_datasources = cfg.CONF.datasources
 
     if deploy_api or deploy_policy or deploy_datasources:
-        if not cfg.CONF.distributed_architecture:
-            sys.exit("ERROR: can only be used with distributed arch")
         if (cfg.CONF.node_id is None and
            not (deploy_api and deploy_policy and deploy_datasources)):
             sys.exit("ERROR: must supply unique node_id")
